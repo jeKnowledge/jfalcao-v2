d86d3fdbdd98c6303ed07c14e315257022b6f8b8
Kabombom <mantonio@student.dei.uc.pt>
2016-04-14 16:59:30 +0100

    Separated files in directories and made minor restructure to code.

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 var app = {};
 
-
 $(document).ready(function () {
-  app.images = ['foto1.JPG', 'foto2.JPG', 'foto3.JPG', 'foto4.JPG','foto5.JPG','foto6.JPG','foto7.JPG'];
-  app.floors = ['piso0.png', 'piso1.png', 'piso2.png', 'piso3.png', 'piso4.png'];
+  app.images = ['images/foto1.JPG', 'images/foto2.JPG', 'images/foto3.JPG', 'images/foto4.JPG', 'images/foto5.JPG', 'images/foto6.JPG', 'images/foto7.JPG'];
+  app.floors = ['images/piso0.png', 'images/piso1.png', 'images/piso2.png', 'images/piso3.png', 'images/piso4.png'];
 
   app.currentImage = 0;
   app.currentFloor = 0;
@@ -11,7 +10,7 @@ $(document).ready(function () {
   /* Preload all images and floors*/
   $.map(app.images, preloadImage);
   $.map(app.floors, preloadImage);
-  changeImage('right', true);
+  changeImage('right');
 
 });
 
@@ -22,25 +21,25 @@ var preloadImage = function (url) {
   img.src = url;
 };
 
-var changeImage = function (direction, firstTime) {
-  if (direction == 'right' && !firstTime) {
+var changeImage = function (direction) {
+  console.log(app.currentImage);
+  if (direction == 'right') {
+    console.log(app.currentImage);
     if (app.currentImage == app.images.length - 1) {
       app.currentImage = 0;
     } else {
       app.currentImage++;
     }
-  } else if (direction == 'left' && !firstTime) {
-    if (app.currentImage == 0) {
+    console.log(app.currentImage);
+  } else if (direction == 'left') {
+    if (app.currentImage === 0) {
       app.currentImage = app.images.length - 1;
     } else {
       app.currentImage--;
     }
-  } else if(direction == 'true' && firstTime){
-    app.currentImage = 0;
   }
-
+  console.log(app.currentImage);
   var image = $('#images');
-
   $(image).fadeOut('slow', function () {
     $(image).css('background', "url('" + app.images[app.currentImage] + "') no-repeat center center fixed");
     $(image).css('background-size', 'cover');
@@ -48,15 +47,15 @@ var changeImage = function (direction, firstTime) {
   });
 };
 
-var changeFloor = function (direction, firstTime) {
-  if (direction == 'right' && !firstTime) {
+var changeFloor = function (direction) {
+  if (direction == 'right') {
     if (app.currentFloor == app.floors.length - 1) {
       app.currentFloor = 0;
     } else {
       app.currentFloor++;
     }
-  } else if (direction == 'left' && !firstTime) {
-    if (app.currentFloor == 0) {
+  } else if (direction == 'left') {
+    if (app.currentFloor === 0) {
       app.currentFloor = app.floors.length - 1;
     } else {
       app.currentFloor--;
@@ -64,7 +63,6 @@ var changeFloor = function (direction, firstTime) {
   }
 
   var floor = $('#floors');
-
   $(floor).fadeOut('slow', function () {
     $(floor).css('background', "url('" + app.floors[app.currentFloor] + "') no-repeat center center fixed");
     $(floor).css('background-size', 'cover');
@@ -74,28 +72,23 @@ var changeFloor = function (direction, firstTime) {
 
 /* FLOORS */
 
-
-$('#floors').bind('tap', function (e) {
-  console.log('deste tap num andar');
-  changeFloor('right');
-});
-
-$('#floors').bind('swiperight', function (e) {
-  changeFloor('left');
-});
-
-$('#floors').bind('swipeleft', function (e) {
-  changeFloor('right');
-});
-
-$('#floors').bind('swipedown', function(e) {
-  $(this).fadeOut(function(){
-    $('#menu').fadeIn();
-  });
+$('floors').bind(function () {
+  if ('tap') {
+    changeFloor('right');
+  } else if ('swiperight') {
+    changeFloor('left');
+  } else if ('swipeleft') {
+    changeFloor('right');
+  } else if ('swipedown') {
+    $(this).fadeOut(function(){
+      $('#menu').fadeIn();
+    });
+  }
 });
 
 
-$('#close-floor p').bind('tap', function (e) {
+
+$('#close-floor p').bind('tap', function () {
   console.log('carregaste no + menu');
   e.stopPropagation();
   $('#floors').fadeOut(function () {
@@ -107,56 +100,55 @@ $('#close-floor p').bind('tap', function (e) {
 
 /* IMAGENS */
 
-
-$('#images').bind('swipeleft', function (e) {
+$('#images').bind('swipeleft', function () {
   changeImage('right');
 });
 
-$('#images').bind('tap', function (e) {
+$('#images').bind('tap', function () {
   changeImage('right');
 });
 
-$('#images').bind('swiperight', function (e) {
+$('#images').bind('swiperight', function () {
   changeImage('left');
 });
 
-$('#images').bind('swipeup', function (e) {
+$('#images').bind('swipeup', function () {
   $(this).fadeOut(function () {
     $('#menu').fadeIn();
   });
 });
 
-$('#menu').bind('swipedown', function (e) {
+$('#menu').bind('swipedown', function () {
   $(this).fadeOut(function () {
     $('#images').fadeIn();
   });
 });
 
-$('#sala').bind('tap', function (e) {
+$('#sala').bind('tap', function () {
   $('#menu').fadeOut(function () {
     changeFloor('right', true);
   });
 });
 
-$('#servico').bind('tap', function (e) {
+$('#servico').bind('tap', function () {
   $('#menu').fadeOut(function () {
     $('#images').fadeIn();
   });
 });
 
-$('#horario').bind('tap', function (e) {
+$('#horario').bind('tap', function () {
   $('#menu').fadeOut(function () {
     $('#images').fadeIn();
   });
 });
 
-$('#professor').bind('tap', function (e) {
+$('#professor').bind('tap', function () {
   $('#menu').fadeOut(function () {
     $('#images').fadeIn();
   });
 });
 
-$('#close-menu p').bind('tap', function (e) {
+$('#close-menu').find('p').bind('tap', function () {
   $('#menu').fadeOut(function () {
     $('#images').fadeIn();
   });
